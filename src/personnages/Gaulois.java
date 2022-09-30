f7b9f07cafbc862ee6b367c2a6c6f341b93306c8
etudianfsi <etudiantfsi@ens.fsi>
2022-09-30 09:47:09 +0200

    fin tp 1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -11,7 +11,33 @@ public class Gaulois {
 		this.force = force;
 	}
 	
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "«" + texte + "»");
+	}
 	
-
-
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";	
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup(force/3);	
+	}
+	
+//	public String toString() { 
+//		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]"; 
+//	} 
+//	public static void main(String[] args) {
+//	
+//		//TODO créer un main permettant de tester la classe Gaulois 
+//		Gaulois asterix = new Gaulois ("Astérix",8);
+//		System.out.println(asterix.getNom());
+//		System.out.println(asterix); 
+//		asterix.parler("Bonjour");
+//	} 	
 }
+
---
+++ src/personnages/Village.java
@@ -0,0 +1,10 @@
+package personnages;
+
+public class Village {
+	private String nom; 
+	private Chef chef; 
+	
+	public String getNom() {
+		return nom;
+	}
+}
