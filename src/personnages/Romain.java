f25ee372e3ee0b1ba033d211f7ea7877955b416e
etudianfsi <etudiantfsi@ens.fsi>
2022-10-18 09:48:10 +0200

    Milieu TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -2,11 +2,14 @@ package personnages;
 
 public class Romain  { 
 	private String nom; 
-	private int force; 
+	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) { 
 		this.nom = nom; 
 		this.force = force; 
+		assert(force>0);
 	} 
 	
 	public String getNom() { 
@@ -28,5 +31,9 @@ public class Romain  {
 		} else { 
 			parler("J'abandonne..."); 
 		} 
-	} 
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", -6);
+	}
 } 
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,8 +3,50 @@ package personnages;
 public class Village {
 	private String nom; 
 	private Chef chef; 
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	
+	public Village(String nom, int nbVillageoisMaximum) { 
+		this.nom = nom; 
+		villageois = new Gaulois[nbVillageoisMaximum];	
+	 } 
+
+	public void setChef(Chef chef) { 
+		this.chef = chef; 
+	 }
 	
 	public String getNom() {
 		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois]=gaulois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int n) {
+		return villageois[n];
+	}
+	
+	public void afficherVillageois () {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois:");
+		for (int i = 0; i < nbVillageois; i++) {
+			Gaulois gaulois = trouverHabitant(i);
+			System.out.println("- " + gaulois.getNom());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Village villageDesIrreductibles =new Village("Village Des Irréductibles", 30);
+//		Gaulois gaulois = villageDesIrreductibles.trouverHabitant(30);
+		Chef abraracourcix = new Chef("Abraracourcix",6,villageDesIrreductibles);
+		villageDesIrreductibles.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		villageDesIrreductibles.ajouterHabitant(asterix);
+//		Gaulois gaulois = villageDesIrreductibles.trouverHabitant(1);
+//		System.out.println(gaulois);
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		villageDesIrreductibles.ajouterHabitant(obelix);
+		villageDesIrreductibles.afficherVillageois();
 	}
 }
